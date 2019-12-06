755da831226c6ebec47b898e9e5c1522715a9526
Ste <sedprc@gmail.com>
2019-12-06 09:40:02 +0100

    added post template

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout';
 import PageTitle from '../components/Pagetitle';
 
@@ -13,6 +13,9 @@ const BlogPage = () => {
 							title
 							date
 						}
+						fields {
+							slug
+						}
 					}
 				}
 			}
@@ -25,13 +28,17 @@ const BlogPage = () => {
 				<h1>
 					the <br />posts <br />page
 				</h1>
+
 				<p>I tend to look for inspiration from observation</p>
 			</PageTitle>
 			<ul>
 				{data.allMarkdownRemark.edges.map((edge) => {
 					return (
 						<li>
-							<h3>{edge.node.frontmatter.title}</h3>
+							<Link to={`/blog/${edge.node.fields.slug}`}>
+								<h3>{edge.node.frontmatter.title}</h3>
+								<h6>{edge.node.frontmatter.date}</h6>
+							</Link>
 						</li>
 					);
 				})}
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-const Blog = () => {
-	return <Layout>This is blog template</Layout>;
+export const query = graphql`
+	query($slug: String!) {
+		markdownRemark(fields: { slug: { eq: $slug } }) {
+			frontmatter {
+				title
+				date
+			}
+			html
+		}
+	}
+`;
+
+const Blog = (props) => {
+	return (
+		<Layout>
+			<h1>{props.data.markdownRemark.frontmatter.title}</h1>
+			<h6>{props.data.markdownRemark.frontmatter.date}</h6>
+			<div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
+		</Layout>
+	);
 };
 
 export default Blog;
