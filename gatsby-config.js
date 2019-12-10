0c63368d2a89351131c69b14929c67a2d4b416d4
Ste <sedprc@gmail.com>
2019-12-10 13:00:56 +0100

    added sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,22 +27,8 @@ module.exports = {
 				name: 'src',
 				path: `${__dirname}/src`
 			}
-		}
-		// 'gatsby-plugin-sharp',
-		// {
-		// 	resolve: 'gatsby-transformer-remark',
-		// 	options: {
-		// 		plugins: [
-		// 			'gatsby-remark-relative-images',
-		// 			{
-		// 				resolve: 'gatsby-remark-images',
-		// 				options: {
-		// 					maxWidth: 750,
-		// 					linkImagesToOriginal: false
-		// 				}
-		// 			}
-		// 		]
-		// 	}
-		// }
+		},
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`
 	]
 };
